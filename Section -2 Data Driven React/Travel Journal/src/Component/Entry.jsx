3b87f17e1b8c31bf331fbdc79f1ae976e04df2e9
shifan-mts <amohamedshifan1@gmail.com>
2025-05-27 20:06:15 +0530

    final touch to the card

--- Section -2 Data Driven React/Travel Journal/src/Component/Entry.jsx
+++ Section -2 Data Driven React/Travel Journal/src/Component/Entry.jsx
@@ -1,21 +1,8 @@
-/**
- * Challenge: Build out the Entry component and render 1 instance of it
- * to the App
- * 
- * For now, just hard-code in the data, which you can find in
- * japan.md so you don't have to type it all out manually :)
- * 
- * Notes:
- * – Only render 1 instance of this Entry component for now
- * – I've pulled in marker.png for the little map marker icon
- *   that goes next to the location name
- * – The main purpose of this challenge is to show you where our limitations
- *   currently are, so don't worry about the fact that you're hard-coding all
- *   this data into the component.
- */
+
 
 function Entry(){
     return <>
+    <div className="card-container">
     <div className="card">
         <div className="card-img">
             <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
@@ -33,12 +20,18 @@ function Entry(){
                 12 Jan, 2021 - 24 Jan, 2021
             </div>
             <div className="details">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists
-            </div>
+                <p>
+                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists
+
+                </p>
+                        </div>
         </div>
         
     </div>
+    </div>
+    
     </>
 }
 
 export default Entry;
+
