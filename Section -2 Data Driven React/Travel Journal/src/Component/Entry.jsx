bdeac2aec79fe3df0fcedc286c04ae68112bf28a
shifan-mts <amohamedshifan1@gmail.com>
2025-06-03 13:19:51 +0530

    added props in travel journal proj

--- Section -2 Data Driven React/Travel Journal/src/Component/Entry.jsx
+++ Section -2 Data Driven React/Travel Journal/src/Component/Entry.jsx
@@ -1,28 +1,28 @@
 
 
-function Entry(){
-    return <>
+function Entry(props){
+
+
+return <>
     <div className="card-container">
     <div className="card">
         <div className="card-img">
-            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
+            <img src={props.img.src} alt={props.img.alt} />
         </div>
         <div className="card-content">
             <div className="card-location">
                 <img src="marker.png" alt="" />
-                <h4>Japan</h4>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu
-" className="gmap">View on Google Maps</a>
+                <h4>{props.country}</h4>
+                <a href={props.googleMapsLink} className="gmap">View on Google Maps</a>
             </div>
-            <h1 className="card-title">Mount Fuji</h1>
+            <h1 className="card-title">{props.title}</h1>
             
             <div className="date">
-                12 Jan, 2021 - 24 Jan, 2021
+                {props.dates}
             </div>
             <div className="details">
                 <p>
-                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists
-
+                {props.text}
                 </p>
                         </div>
         </div>
