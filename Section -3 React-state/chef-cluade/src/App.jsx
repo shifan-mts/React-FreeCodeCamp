0beaa12b207764c9df7cc3739630dce13aa0fbd7
shifan-mts <amohamedshifan1@gmail.com>
2025-06-05 11:50:01 +0530

    header added

---
+++ Section -3 React-state/chef-cluade/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+
+
+ function App() {
+  return (
+      <Header />
+  
+  )
+}
+
+export default App;
---
+++ Section -3 React-state/chef-cluade/src/Components/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav(){
+    return <nav className="nav-bar">
+        <ul className="nav-elements">
+            <li>
+                <img src="chef-claude-icon.png" alt="" className="logo"/>
+            </li>
+            <li>
+                <h4 className="nav-title">Chef Claude</h4>
+            </li>
+        </ul>
+    </nav>
+ 
+    
+}
+
+export default Nav;
