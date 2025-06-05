a90583d37d6e35b9f64752a23779ed3fc3c67633
shifan-mts <amohamedshifan1@gmail.com>
2025-06-05 22:40:33 +0530

    this is shif version

--- Section -3 React-state/statePractice/App.jsx
+++ Section -3 React-state/statePractice/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function App() {
     /**
@@ -6,13 +6,15 @@ export default function App() {
      * Create state to track our count value (initial value is 0)
      * Don't forget to replace the hard-coded "0" with your new state
      */
+
+    let [count,setCount] = useState(0);
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">0</h2>
-                <button className="plus" aria-label="Increase count">+</button>
+                <button className="minus" aria-label="Decrease count" onClick={()=>setCount(count-1)}>–</button>
+                <h2 className="count">{count}</h2>
+                <button className="plus" aria-label="Increase count" onClick={()=>setCount(count+1)}>+</button>
             </div>
         </main>
     )
