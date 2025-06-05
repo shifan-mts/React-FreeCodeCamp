bc05b30ca09974acb56f99a9ec5961be95336280
shifan-mts <amohamedshifan1@gmail.com>
2025-06-05 23:26:22 +0530

    this is my solution

--- Section -3 React-state/Toggling state/App.jsx
+++ Section -3 React-state/Toggling state/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function App() {
     /**
      * Challenge: 
@@ -6,11 +8,27 @@ export default function App() {
      *   boolean value (true -> false, false -> true)
      * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
      */
+    let [going,goingOut] = useState(true);
+    let [permission,setPermission] = useState("Yes");
+    function handleSubmit(){
+        
+        if(going==true){
+            
+            goingOut(false);
+            setPermission("No");
+        }
+        else{
+         
+            goingOut(true);
+            setPermission("Yes");
+        }
+        
+    }
 
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value">Yes</button>
+            <button className="value" onClick={handleSubmit}>{permission}</button>
         </main>
     )
 }
