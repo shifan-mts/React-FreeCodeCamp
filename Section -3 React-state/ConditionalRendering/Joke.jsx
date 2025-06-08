dc9f200f496e9c85d32ef373e92f7315a6a0e6ef
shifan-mts <amohamedshifan1@gmail.com>
2025-06-08 22:15:31 +0530

    Added Click me button which toggles the joke

---
+++ Section -3 React-state/ConditionalRendering/Joke.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export default function Joke(props) {
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+    const [isShown,toggleShown] = useState(false);
+    console.log(isShown)
+    function handleClick(){
+        toggleShown(
+            n => !n
+        )
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            <p>{isShown && props.punchline}</p>
+            <button onClick={handleClick}> Click me</button>
+            <hr />
+        </div>
+    )
+}
